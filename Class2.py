20f26d6d24eb3a9ae089d1df564bd1c212a831ee
Marina Cantarelli <macbarroca@gmail.com>
2024-07-13 16:52:48 -0300

    Mine first class

---
+++ Class2.py
@@ -0,0 +1,12 @@
+#Mine first class
+
+class Employees:
+    name = 'Helena'
+    subname = 'Barroca'
+    date_birth = '12/10/1940'
+
+
+user1 = Employees()
+print(user1.name)
+print(user1.subname)
+print(user1.date_birth)
