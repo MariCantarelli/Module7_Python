daf589286be76dc1244a39b7c315a165147d8292
Marina Cantarelli <macbarroca@gmail.com>
2024-07-13 16:48:29 -0300

    Classes

---
+++ Class1.py
@@ -0,0 +1,10 @@
+#Classes
+
+#Utilizadas para criar objetos (instances)
+#Objetos são partes de uma class (instancias)
+#Classes são utilizadas para agrupar dados e funções, podendo reutilizar
+
+name = 'Andre'
+#print(type(name))
+name_lower = name.lower()
+print(name_lower)
